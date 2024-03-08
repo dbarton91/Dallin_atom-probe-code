54d6c129ae1cf770603095dacb1d27920b68b95b
Dallin Barton <dallin.barton@pnnl.gov>
2024-03-07 16:29:15 -0800

    added pu

--- H_study/1D_concentration_profile/1D_concentration_profile_Pu.py
+++ H_study/1D_concentration_profile/1D_concentration_profile_Pu.py
@@ -6,7 +6,7 @@ Created on Sat Mar 19 16:57:59 2022
 @author: bart924
 """
 
-import pandas as pd # for reading csv's and creating the dataframe
+import pandas as pd  # for reading csv's and creating the dataframe
 import matplotlib.pyplot as plt #for plotting
 import os #for changing the directory
 import mpl_toolkits.axisartist as axisartist
@@ -38,13 +38,13 @@ rcParams['ytick.minor.pad'] = 6.0
 plt.rcParams["font.family"] = "Arial"
 
 howmanyplots = 1 #numberofplotsyou want
-currentdirectory = r'Z:\Experimental_results\APT\NETL\DT2\liftout2\R5115_00474'
-csvfilename1 = "R5115_00474_SEMrecon_v1 - Cylinder ID 1 - 1D Concentration Profile - Z- axis.csv"
+currentdirectory = r'C:\Users\bart924\Desktop'
+csvfilename1 = "R5115_00738_tipprofile_01 - Cylinder ID 0 - 1D Concentration Profile - Z- axis.csv"
 
 
 os.chdir(currentdirectory)
-savefilename = "R5115_00474_SEMrecon_v1 - Cylinder ID 1 - 1D Concentration Profile - Z- axis" #defaults with .png extension.
-savefilename = "test"
+savefilename = "test7" #defaults with .png extension.
+
 resolution = 1200 # resolution for the saved figure in dots per inch. 1200 is usually good.
 lowresolution = 150 # resolution for ppt slides
 
@@ -68,7 +68,13 @@ df1 = df1.iloc[1:]
 # Reset the index after removing the first row
 df1 = df1.reset_index(drop=True)
 
-words_to_remove = ["%", "atom", " ","Atom","(nm)"]
+# typeincell= type(df1.at[0,0])
+#
+# print(typeincell)
+
+words_to_remove = ["%", "atom", " ", "Atom", "(nm)"]
+
+
 
 # Remove words from column names
 for word in words_to_remove:
@@ -77,31 +83,31 @@ for word in words_to_remove:
 
 # df1 = df1.dropna()
 
-numeric_columns = ['Cr', 'Ni', 'Co', 'Mo', 'Count','CrSigma','NiSigma','CoSigma','MoSigma','Distance']
+numeric_columns = ['Pu', 'Ga', 'O', 'Fe', 'Count', 'PuSigma', 'GaSigma', 'OSigma', 'FeSigma', 'Distance']
 df1[numeric_columns] = df1[numeric_columns].apply(pd.to_numeric, errors='coerce').fillna(0)
 
 
-df1["Crcounts"] = df1["Cr"] * df1["Count"] / 100
-df1["Nicounts"] = df1["Ni"] * df1["Count"] / 100
-df1["Cocounts"] = df1["Co"] * df1["Count"] / 100
-df1["Mocounts"] = df1["Mo"] * df1["Count"] / 100
-df1["TotalCounts"] = df1["Crcounts"] + df1["Nicounts"] + df1["Cocounts"] + df1["Mocounts"]
+df1["Pucounts"] = df1["Pu"] * df1["Count"] / 100
+df1["Gacounts"] = df1["Ga"] * df1["Count"] / 100
+df1["Ocounts"] = df1["O"] * df1["Count"] / 100
+df1["Fecounts"] = df1["Fe"] * df1["Count"] / 100
+df1["TotalCounts"] = df1["Pucounts"] + df1["Gacounts"] + df1["Ocounts"] + df1["Fecounts"]
 
-df1["Cr"] = df1["Crcounts"] / df1["TotalCounts"] * 100
-df1["Ni"] = df1["Nicounts"] / df1["TotalCounts"] * 100
-df1["Co"] = df1["Cocounts"] / df1["TotalCounts"] * 100
-df1["Mo"] = df1["Mocounts"] / df1["TotalCounts"] * 100
+df1["Pu"] = df1["Pucounts"] / df1["TotalCounts"] * 100
+df1["Ga"] = df1["Gacounts"] / df1["TotalCounts"] * 100
+df1["O"] = df1["Ocounts"] / df1["TotalCounts"] * 100
+df1["Fe"] = df1["Fecounts"] / df1["TotalCounts"] * 100
 
 
 #List the x range that you would like to plot
-xminrange = 8
-xmaxrange = 25.5
+xminrange = 75
+xmaxrange = 150
 #Officially, plot only normalized plots. In this code it is against the highest peak (Highest peak is "1" and the rest is a fraction of that)
-yminrange = 18 # Low enough to see the background floor but not waste space
-ymaxrange = 45 #1.1 for the tiny bit of space up top.
+yminrange = 60 # Low enough to see the background floor but not waste space
+ymaxrange = 90 #1.1 for the tiny bit of space up top.
 
 yminrangesmall = 0.01
-ymaxrangesmall = 11
+ymaxrangesmall = 15
 
 # df1.rename(index=str,columns={"Distance (nm)":"Distance"},inplace=True)
 df1["Distance"] = df1["Distance"] - xminrange
@@ -111,10 +117,10 @@ df1 = df1[df1.Distance <= xmaxrange]
 
 fig, (ax1,ax2) = plt.subplots(2, 1, sharex=True,gridspec_kw={'height_ratios': [2, 1]})
 
-plotCo = df1.plot("Distance", "Co", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax1, color = [(0,0,1)], label = "Co", legend = False,fontsize=20)
-plotCr = df1.plot("Distance", "Cr", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax1, color = [(1,0,1)], label = "Cr", legend = False,fontsize=20)
-plotNi = df1.plot("Distance", "Ni", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax1, color = [(0,204/255,0)], label = "Ni", legend = False,fontsize=20)
-plotMo = df1.plot("Distance", "Mo", xlim=[0,xmaxrange], ylim=[yminrangesmall,ymaxrangesmall], ax = ax2, color = [(1,0,0)], label = "Mo", legend = False,fontsize=20)
+plotPu = df1.plot("Distance", "Pu", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax1, color = [(0,0,0)], label = "Pu", legend = False,fontsize=20)
+plotGa = df1.plot("Distance", "Ga", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax2, color = [(232/255,119/255,34/255)], label = "Ga", legend = False,fontsize=20)
+plotO = df1.plot("Distance", "O", xlim=[0,xmaxrange], ylim=[yminrange,ymaxrange], ax = ax2, color = [(108/255, 216/255, 255/255)], label = "O", legend = False,fontsize=20)
+plotFe = df1.plot("Distance", "Fe", xlim=[0,xmaxrange], ylim=[yminrangesmall,ymaxrangesmall], ax = ax2, color = [(1,0,0)], label = "Fe", legend = False,fontsize=20)
 # plotHtwo = df1.plot("Distance", "He %", xlim=[0,xmaxrange], ylim=[yminrangesmall,ymaxrangesmall], ax = ax2, color = [(255/255,0,0)], label = "$^{2}$H", legend = False,fontsize=20,yerr="He % Sigma")
 # plotO = df1.plot("Distance", "O %", xlim=[0,xmaxrange], ylim=[yminrangesmall,ymaxrangesmall], ax = ax2, color = [(0,0,1)], label = "O", legend = False,fontsize=20,yerr="O % Sigma")
 
@@ -131,10 +137,10 @@ fig.text(0.02, 0.48, 'Concentration [at. %]', ha='center', va='center', rotation
 plt.subplots_adjust(hspace=0.17)
 plt.xlabel("Distance [nm]", fontsize=20, weight = 'bold')
 
-ax1.fill_between(df1["Distance"],df1["Co"]+df1["CoSigma"],df1["Co"]-df1["CoSigma"], color = [(192/255,192/255,1)])
-ax1.fill_between(df1["Distance"],df1["Cr"]+df1["CrSigma"],df1["Cr"]-df1["CrSigma"], color = [(1,192/255,1)])
-ax1.fill_between(df1["Distance"],df1["Ni"]+df1["NiSigma"],df1["Ni"]-df1["NiSigma"], color = [(205/255,235/255,218/255)] )
-ax2.fill_between(df1["Distance"],df1["Mo"]+df1["MoSigma"],df1["Mo"]-df1["MoSigma"], color = [(255/255,205/255,205/255)] )
+ax1.fill_between(df1["Distance"],df1["Pu"]+df1["PuSigma"],df1["Pu"]-df1["PuSigma"], color = [(192/255,192/255,1)])
+ax1.fill_between(df1["Distance"],df1["Ga"]+df1["GaSigma"],df1["Ga"]-df1["GaSigma"], color = [(1,192/255,1)])
+ax1.fill_between(df1["Distance"],df1["O"]+df1["OSigma"],df1["O"]-df1["OSigma"], color = [(205/255,235/255,218/255)] )
+ax2.fill_between(df1["Distance"],df1["Fe"]+df1["FeSigma"],df1["Fe"]-df1["FeSigma"], color = [(255/255,205/255,205/255)] )
 
 
 ax1.tick_params(axis="x", bottom=False, top=True, labelbottom=False, labeltop=False, direction="in")
